428c33a79f531c7f862d366bdbd6480dc8f7131d
tracyntimothy <TNT014@mymail.tc3.edu>
2020-01-22 14:25:12 -0500

    Tracy Timothy lab submission for module 1

---
+++ module-1/HelloWorld.java
@@ -0,0 +1,12 @@
+// file name must match the public class identifier
+public class HelloWorld{
+
+		// main method is the starting point of any Java app
+		public static void main(String[] args){
+
+			// built in terminal printing utility
+			System.out.println("Hello World!");
+
+
+		} // end of main
+} // end of class
---
+++ module-1/HelloWorld2.java
@@ -0,0 +1,14 @@
+// file name must match the public class identifier
+public class HelloWorld2{
+
+		// main method is the starting point of any Java app
+		//Run|Debug
+		public static void main(String[] args){
+
+			for (String s : args)
+
+				System.out.println("Hello " + s );
+				System.out.println("Nice work processing the arguments");
+
+		} // end of main
+} // end of class
