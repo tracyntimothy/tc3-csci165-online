4efbd4f7be9be56858059d5825363c49c3827203
tracyntimothy <TNT014@mymail.tc3.edu>
2020-01-23 19:11:34 -0500

    lab submission for module 1

--- module-1/HelloWorld2.java
+++ module-1/HelloWorld2.java
@@ -2,12 +2,12 @@
 public class HelloWorld2{
 
 		// main method is the starting point of any Java app
-		//Run|Debug
+		
 		public static void main(String[] args){
 
-			
+			// Send message to terminal including arguments added at the command line
 				System.out.println("Hello " + " " + args[0] + " " + args[1] + ".  Nice work processing those arguments!");
-				//System.out.println("Nice work processing the arguments");
+				
 
 		} // end of main
 } // end of class
